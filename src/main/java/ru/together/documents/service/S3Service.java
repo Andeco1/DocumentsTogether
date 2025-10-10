6c7c48c15cc6f6b65d0e719d9e9c94165d465068
Yan <Andeco12@yandex.ru>
2025-10-10 03:04:09 +0300

    S3 fixed

--- src/main/java/ru/together/documents/service/S3Service.java
+++ src/main/java/ru/together/documents/service/S3Service.java
@@ -10,6 +10,7 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
@@ -48,6 +49,7 @@ public class S3Service {
                     .key(key)
                     .contentType(file.getContentType())
                     .contentLength(file.getSize())
+                    .acl(ObjectCannedACL.PUBLIC_READ) // Make the object publicly readable
                     .build();
 
             s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(
@@ -85,4 +87,5 @@ public class S3Service {
         }
         return "documents/" + UUID.randomUUID().toString() + extension;
     }
+
 }
